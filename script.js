b376a0161ebacbd64202f7b5a0c0e4cf3e353bbf
Emmanuel C. Jemeni <52603291+tallCoder02@users.noreply.github.com>
2021-08-14 21:27:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let firstCard = 10;
+let secondCard = 4;
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let newCardBtn = document.querySelectorAll("button")[1];
+
+cardsEl.textContent = "Cards: "
+
+function startGame(){
+    renderGame();
+}
+
+function renderGame(){\
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21){
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message;
+    newCardBtn.removeAttribute('disabled');
+}
+
+function newCard(){
+    let newCard = Math.ceil(Math.random() * 9);
+    sum += newCard;
+    cardsEl.textContent += newCard + "  ";
+    sumEl.textContent = "Sum: " + sum;
+    renderGame();
+}
